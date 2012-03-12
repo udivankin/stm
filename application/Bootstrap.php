6d663625c2d70cb5a7a0ab66e20ca9bcdd5e7f5b
Alexey Udivankin <alexey@udivankin.ru>
2012-03-13 02:09:21 +0400

    todo

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -1,5 +1,11 @@
 <?php
 
+// TODO: ADD STORIES VIA ZEND FORMS & AJAX (if possible)
+// TODO: EDIT STORIES
+// TODO: UPDATING STATUS W/LOGIC VIA AJAX
+// TODO: STORY ID HIGHLIGHTING (maybe like #tag in twitter)
+// TODO: PHPUNIT TESTING
+
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
