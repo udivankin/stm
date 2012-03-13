60ff0c61408b363250bf75fb6e6f6af0b132776c
Alexey Udivankin <alexey@udivankin.ru>
2012-03-13 15:43:27 +0400

    todo cleanup :)

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,13 +2,13 @@
 
 // TODO: ADD STORIES VIA ZEND FORMS & AJAX (if possible)
 // TODO: EDIT STORIES
-// TODO: STORY RATING (ensure that shown only for Author, only when story is Finished)
-// TODO: IF RATING IS SET - DISPLAY it as text
 // TODO: STORY ID HIGHLIGHTING (maybe like #tag in twitter)
 // TODO: SQL security - GET only related stories, UPDATE/DELETE only own STORIES, DELETE ONLY OWN COMMENTS
 // TODO: Set disabled status to delete and edit buttons if user is not owner
 // TODO: PHPUNIT TESTING
 // 
+// DONE: STORY RATING (ensure that shown only for Author, only when story is Finished)
+// DONE: IF RATING IS SET - DISPLAY it as text 
 // DONE: MAKE ACCEPT REJECT BUTTONS ACTIVE only when story is finished and only for Author (invisible for officer)
 // DONE: UPDATING STATUS W/LOGIC VIA AJAX 
 // DONE: IF not ajax request, make a redirect
